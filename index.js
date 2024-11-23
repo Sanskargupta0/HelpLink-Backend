7dda21353661d6dc5321481426f4bda1eccb100d
Sanskargupta0 <sanskar362002@gmail.com>
2024-11-23 21:46:21 +0530

    Added: SuperToken api route

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const { errorHandler } = require('supertokens-node/framework/express');
+const { middleware } = require('supertokens-node/framework/express');
+const supertokens = require('./server/utils/SuperTokens')
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type', ...supertokens.getAllCORSHeaders()],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
+  
+
+app.use(middleware());
 
 app.get('/', (req, res) => {
     res.status(200).send('Health check passed');
+});
+
+app.use(errorHandler());
+
+app.use((err, req, res, next) => {
+   res.status(500).send("Internal server error"+err.message);
 });
 
 app.listen(8080, () => {
---
+++ server/utils/SuperTokens.js
@@ -0,0 +1,48 @@
+const supertokens = require("supertokens-node");
+const Session = require("supertokens-node/recipe/session");
+const EmailPassword = require("supertokens-node/recipe/emailpassword");
+const EmailVerification = require("supertokens-node/recipe/emailverification");
+const ThirdParty = require("supertokens-node/recipe/thirdparty");
+
+
+
+supertokens.init({
+    framework: "express",
+    supertokens: {
+        // https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+        connectionURI: "https://try.supertokens.com",
+        // apiKey: <API_KEY(if configured)>,
+    },
+    appInfo: {
+        appName: "Delhivery",
+        apiDomain: "http://localhost:8080",
+        websiteDomain: "http://localhost:5173",
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth"
+    },
+    recipeList: [
+        EmailPassword.init(),
+        EmailVerification.init({
+            mode: "REQUIRED", // or "OPTIONAL"
+          }),
+        ThirdParty.init({
+            // We have provided you with development keys which you can use for testing.
+            // IMPORTANT: Please replace them with your own OAuth keys for production use.
+            signInAndUpFeature: {
+                providers: [{
+                    config: {
+                        thirdPartyId: "google",
+                        clients: [{
+                            clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+                            clientSecret: "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"
+                        }]
+                    }
+                }],
+            }
+        }),
+        Session.init() // initializes session features
+    ]
+});
+
+
+module.exports = supertokens;

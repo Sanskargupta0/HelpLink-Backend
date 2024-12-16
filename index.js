660b9b548e2cc9bdff0255a67de2d969e3e788bd
Sanskargupta0 <sanskar362002@gmail.com>
2024-12-16 21:35:25 +0530

    Added: init socket io connection

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
 const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const connectDB = require("./server/Utils/db");
+const socketIo = require("socket.io");
 const cors = require("cors");
 const { errorHandler } = require("supertokens-node/framework/express");
 const { middleware } = require("supertokens-node/framework/express");
@@ -8,23 +15,49 @@ const {
   verifySession,
 } = require("supertokens-node/recipe/session/framework/express");
 const userRouter = require("./server/Routers/userData-routes");
-const connectDB = require("./server/Utils/db");
-const dotenv = require("dotenv");
-dotenv.config();
-const app = express();
-
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "1mb", extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     allowedHeaders: ["Content-Type", ...supertokens.getAllCORSHeaders()],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
+const io = socketIo(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      ...supertokens.getAllCORSHeaders(),
+    ],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("authenticate", (userId) => {
+    socket.join(userId);
+    console.log(`User ${userId} connected`);
+  });
+
+  socket.on("disconnect", () => {
+    socket.leave(socket.id);
+    console.log("User disconnected");
+  });
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Health check passed");
@@ -42,7 +75,7 @@ app.use(errorMiddleware);
 
 const port = process.env.PORT || 3001;
 connectDB().then(() => {
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 });
